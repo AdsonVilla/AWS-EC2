d515b3ec38eb4f26c24a4ca51a1a7a26d633f625
Adson <adson23_@hotmail.com>
2024-06-25 18:18:09 -0300

    Creating EC2 instance with an IAM role

--- ec2-instance.py
+++ ec2-instance.py
@@ -1,2 +1,24 @@
 import boto3 # type: ignore
-print(boto3.__version__)
+
+
+def create_ec2_instance():
+    
+    session = boto3.Session(region_name='us-east-1')  
+    
+    ec2 = session.resource('ec2')
+
+    instance = ec2.create_instances(
+        ImageId='ami-01b799c439fd5516a',  # AMI Linux 2023
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',  # tipo de instância desejado
+        KeyName='Chave2AWSEC2',  # nome da sua chave EC2
+        IamInstanceProfile={
+            'Name': 'Admin-role-for-ec2'
+        }
+    )
+
+    print("Instância criada com ID:", instance[0].id)
+
+if __name__ == "__main__":
+    create_ec2_instance()
